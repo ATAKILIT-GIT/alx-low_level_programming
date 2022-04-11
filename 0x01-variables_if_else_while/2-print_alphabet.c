e0dd25071bb2ae751a91cc05c03b3fcaae35f4cf
ATAKILIT-GIT <atakilt100@gmail.com>
2022-04-11 11:51:50 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+	char alphabe;
+	for(alphabe = 'a' ;alphabet <= 'z'; alphabet++)
+	{
+putchar(alphabet);
+	}
+}
+putchar('\n');
+return (0);
+
+}
+
