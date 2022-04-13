5d66553aa06accceab08013dce7d39c201f095a4
ATAKILIT-GIT <atakilt100@gmail.com>
2022-04-13 16:16:25 +0300

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,11 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
 
 int main(void)
 {
-
 int i = 0;
 
 positive_or_negative(i);
@@ -16,4 +14,3 @@ positive_or_negative(i);
 return (0);
 
 }
-
